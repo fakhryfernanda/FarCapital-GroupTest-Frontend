a0c38e70e74b01a96d3482e1b44fa5b5e94ca84f
fakhryfernanda <mff023@gmail.com>
2022-12-24 10:35:37 +0700

    (+) route untuk admin

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('index');
-// });
 
 Route::get('/', function () {
     return view('aspirasi');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [AdminController::class, 'login']);
+Route::post('/login', [AdminController::class, 'authenticate']);
 
 
 Route::get('admin/dashboard', function () {
