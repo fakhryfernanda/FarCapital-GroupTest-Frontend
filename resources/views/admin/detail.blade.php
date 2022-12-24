d0224b78e07b5f1dc0cd16401c6d5b6bd914a916
areef44 <areef.empat.empat@gmail.com>
2022-12-24 10:22:40 +0700

    (+)Halaman Detail Aspirasi

---
+++ resources/views/admin/detail.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.templates')
+
+@section('title','Halaman Form Inspirasi')
+
+@section('detail')
+
+<h3 class="text-center py-5">Halaman Detail Aspirasi</h3>
+<div class="card mx-auto" style="width: 80%">
+  <img class="card-img-top" src="..." alt="Foto Aspirasi">
+  <div class="card-body">
+    <h5 class="card-title">Nama Aspirator</h5>
+    <p class="card-text">Cerita Aspirator</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Tanggal</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Kembali</a>
+  </div>
+</div>
+
+@endsection
