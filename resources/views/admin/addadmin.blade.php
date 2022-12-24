1d9ead6ceecd0c0291c5340a2344c1c5e6733f46
areef44 <areef.empat.empat@gmail.com>
2022-12-24 13:55:11 +0700

    menambahkan halaman formtambah admin dan kelola admin

---
+++ resources/views/admin/addadmin.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.templates')
+
+@section('title','Tambah admin')
+
+@section('addadmin')
+
+<h3>Form Tambah Admin</h3>
+<form action="" method="post" enctype="multipart/form-data">
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+  @csrf
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+        <label class="form-label" for="form6Example1" > Masukan Email</label>
+        <input type="email" id="form6Example1" class="form-control" name="email" required/>      
+      </div>
+    </div>
+
+  <!-- Text input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form6Example3" > Password :</label>
+    <input type="password" id="form6Example3" class="form-control" name="password"required/>
+  </div>
+
+  
+    <!-- Email input -->
+  <div class="form-outline mb-4">
+    <label class="form-label" for="form6Example5">Verifikasi Password</label>
+    <input type="password" id="form6Example3" class="form-control" name="password"required/>
+  </div>
+
+  <!-- Submit button -->
+  <div>
+     <button type="submit" class="btn btn-primary">Kirim</button>
+  </div>
+ 
+
+</form>
+    
+@endsection
